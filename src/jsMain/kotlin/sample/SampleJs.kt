92effbcd9bfce7108b4a45e03811bb5a6c55cd77
natanfudge <natan.lifsiz@gmail.com>
2019-07-01 12:07:10 +0300

    update artifact names

--- src/jsMain/kotlin/sample/SampleJs.kt
+++ src/jsMain/kotlin/sample/SampleJs.kt
@@ -1,9 +1,2 @@
 package sample
 
-actual class Sample {
-    actual fun checkMe() = 12
-}
-
-actual object Platform {
-    actual val name: String = "JS"
-}
--- src/mingwMain/kotlin/sample/SampleMingw.kt
+++ src/mingwMain/kotlin/sample/SampleMingw.kt
@@ -1,9 +1,2 @@
 package sample
 
-actual class Sample {
-    actual fun checkMe() = 7
-}
-
-actual object Platform {
-    actual val name: String = "Native"
-}
